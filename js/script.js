2344bdcc5ec4a43f1e0bfe87cfb6831caafce98f
Arindam Bhowal <arindambhowal10@gmail.com>
2021-11-26 20:47:33 +0530

    Added collab section

--- js/script.js
+++ js/script.js
@@ -74,3 +74,27 @@ function shiftRight() {
     }, 500);
 
 }
+
+// ---------------------Sponsers Section -----------------
+$(document).ready(function(){
+	$('.customer-logos').slick({
+		slidesToShow: 6,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 1500,
+		arrows: false,
+		dots: false,
+		pauseOnHover:false,
+		responsive: [{
+			breakpoint: 768,
+			setting: {
+				slidesToShow:4
+			}
+		}, {
+			breakpoint: 520,
+			setting: {
+				slidesToShow: 3
+			}
+		}]
+	});
+});
